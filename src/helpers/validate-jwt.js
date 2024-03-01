8fcef364beeb6db6faa6e560753c868301010162
Diego Baeza <diego.icinf@gmail.com>
2024-03-01 01:37:51 -0300

    correcion validacion de token

--- src/helpers/validate-jwt.js
+++ src/helpers/validate-jwt.js
@@ -3,24 +3,32 @@ import jwt from "jsonwebtoken";
 const secretKey = process.env.SECRETORPRIVATEKEY;
 
 function validarToken(req, res, next) {
-    // Verificar si existe el token en el header de la solicitud
-    const token = req.headers['authorization'].replace('Bearer ', '');
 
-    console.log(token);
-    if (!token) {
+
+    // Verificar si existe el token en el header de la solicitud
+    const token_res = req.headers['authorization'];
+
+    console.log(token_res);
+    if (!token_res) {
+
       return res.status(401).json({ error: 'Acceso denegado. Token no proporcionado.' });
+
+    }else{
+
+      const token = req.headers['authorization'].replace('Bearer ', '');
+
+      // Verificar y decodificar el token
+      jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+          return res.status(403).json({ error: 'Acceso denegado. Token inválido.' });
+        } else {
+          // El token es válido, adjuntar los datos decodificados a la solicitud
+          req.usuario = decoded;
+          next();
+        }
+      });
+
     }
-  
-    // Verificar y decodificar el token
-    jwt.verify(token, secretKey, (err, decoded) => {
-      if (err) {
-        return res.status(403).json({ error: 'Acceso denegado. Token inválido.' });
-      } else {
-        // El token es válido, adjuntar los datos decodificados a la solicitud
-        req.usuario = decoded;
-        next();
-      }
-    });
   }
 
 

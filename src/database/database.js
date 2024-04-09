e9c9ae3ce612ffe32b2e5f25232af5c38b80d11a
Diego Baeza <diego.icinf@gmail.com>
2024-04-09 01:17:36 -0400

    nuevo formato conexcion a base de datos

--- src/database/database.js
+++ src/database/database.js
@@ -1,17 +1,28 @@
 import Sequalize from 'sequelize'
 import config from "../config.js";
 
-export const sequelize =  new Sequalize(
-    config.database,
-    config.user,
-    config.password,
-      {
-        host : config.host,
-        dialect: "postgres",
-        dialectOptions: {
-            ssl: {
-              require: true,
-              rejectUnauthorized: false // solo si estás usando self-signed certificate
-            }
-        }
-})
+// export const sequelize =  new Sequalize(
+//     config.database,
+//     config.user,
+//     config.password,
+//       {
+//         host : config.host,
+//         dialect: "postgres",
+//         dialectOptions: {
+//             ssl: {
+//               require: true,
+//               rejectUnauthorized: false // solo si estás usando self-signed certificate
+//             }
+//         }
+// })
+
+
+export const sequelize = new Sequalize({
+  dialect: 'postgres',
+  host: config.host,
+  port: config.port_bd,
+  username: config.user,
+  password: config.password,
+  database: config.database,
+  logging: false // Opcional: deshabilita los registros de consultas SQL
+});

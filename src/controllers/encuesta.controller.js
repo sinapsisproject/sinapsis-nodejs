341093aa90067bc2ed338ee9668230fb1b3366ba
sinapsisproject <sinapsisproject2024@gmail.com>
2024-10-14 17:54:56 -0300

    respuestas encuestas

--- src/controllers/encuesta.controller.js
+++ src/controllers/encuesta.controller.js
@@ -3,93 +3,83 @@ import { encuesta_pregunta } from '../models/encuesta_pregunta.model.js';
 import { encuesta_alternativa } from '../models/encuesta_alternativa.model.js';
 import { encuesta_respuesta } from '../models/encuesta_respuesta.model.js';
 
-const getPreguntasByIdEncuesta = async (req, res) => {
+
+const getPreguntasByIdEncuesta = async(req , res) => {
+
     try {
+        
         const { id } = req.params;
 
-        const data = await encuesta.findAll({
-            where: { id },
+        const data =  await encuesta.findAll({
+            where: {
+                id : id
+            },
             include: [
                 {
-                    model: encuesta_pregunta,
-                    include: [{ model: encuesta_alternativa }],
-                },
+                    model : encuesta_pregunta,
+                    include : [
+                        {
+                            model: encuesta_alternativa
+                        }
+                    ]
+                }
             ],
-            order: [[encuesta_pregunta, encuesta_alternativa, 'id', 'DESC']],
-        });
+            order : [
+                [encuesta_pregunta, encuesta_alternativa ,'id' , 'DESC']
+            ]
+
+        })
 
         res.json({
-            status: true,
-            response: data,
-        });
+            "status" : true,
+            "response" : data
+        })
+
     } catch (error) {
-        res.status(500).json({
-            status: false,
-            msg: 'Error al ejecutar la consulta',
-            error,
-        });
+        res.json({
+            "status" : false,
+            "msg"    : 'Error al ejecutar la consulta',
+            "error"  : error
+        })        
     }
-};
 
-const insertResponseFormularios = async (req, res) => {
-    try {
-        const { respuestas, id_encuesta } = req.body;
-        const id_usuario = req.usuario.uid;
+     
+}
 
-        // Verificar si el usuario ya tiene respuestas para esta encuesta
-        const existingRespuestas = await encuesta_respuesta.findOne({
-            where: { id_usuario },
-            include: [
-                {
-                    model: encuesta_alternativa,
-                    include: [
-                        {
-                            model: encuesta_pregunta,
-                            where: { id_encuesta }, // Validar por encuesta específica
-                        },
-                    ],
-                },
-            ],
-        });
 
-        if (existingRespuestas) {
-            return res.status(400).json({
-                status: false,
-                response: 'Ya has respondido esta encuesta',
-            });
-        }
+const insertResponseFormularios = async(req , res) => {
 
-        // Insertar las nuevas respuestas
-        if (respuestas.length > 0) {
-            await Promise.all(
-                respuestas.map(async (id_respuesta) => {
-                    await encuesta_respuesta.create({
-                        id_encuesta_alternativa: id_respuesta,
-                        id_usuario,
-                    });
+    const {respuestas} = req.body;
+    const id_usuario = req.usuario.uid;
+
+    if(respuestas.length > 0){
+        await Promise.all(
+            respuestas.map(async (id_respuesta) => {
+
+                const response_enc = await encuesta_respuesta.create({
+                    id_encuesta_alternativa : id_respuesta,
+                    id_usuario
                 })
-            );
 
-            res.json({
-                status: true,
-                response: 'Respuestas guardadas correctamente',
-            });
-        } else {
-            res.status(400).json({
-                status: false,
-                response: 'No hay respuestas para guardar',
-            });
-        }
-    } catch (error) {
-        res.status(500).json({
-            status: false,
-            msg: 'Error al guardar las respuestas',
-            error,
-        });
+            }
+        ))
+
+        res.json({
+            "status" : true,
+            "response" : "Datos ingresados"
+        })
+
+    }else{
+        res.json({
+            "status" : false,
+            "response" : "No hay respuestas"
+        })
     }
-};
+
+}
+
 
 export const methods = {
     getPreguntasByIdEncuesta,
-    insertResponseFormularios,
-};
+    insertResponseFormularios
+}
